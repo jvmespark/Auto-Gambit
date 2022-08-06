ede8d64b3a9f60fbc0b2354ad47794c97b3b36a1
James Park <jamesdparkny@gmail.com>
2022-08-06 17:38:04 -0400

    piece movement

--- chess_engine.py
+++ chess_engine.py
@@ -12,6 +12,8 @@ class gamestate():
                      ]
         self.white_move = True
         self.move_log = []
+        self.move_functions = {'p': self.get_pawn_moves, 'q': self.get_queen_moves, 'k':self.get_king_moves, 
+                               'n':self.get_knight_moves, 'r':self.get_rook_moves, 'b':self.get_bishop_moves}
 
     def make_move(self, move):
         #make old spot blank
@@ -31,51 +33,181 @@ class gamestate():
             self.white_move = not self.white_move
 
     def get_valid_moves(self):
-        #filters moves
-        valid_moves = []
-        bool king_is_safe = True
-        get_all_possible_moves()
-        #make move
-        #generate opp moves
-        #see if moves attacks king
-        #king safe = valid = add to list
-        #return list of valid moves only
+        return self.get_all_possible_moves()
 
     def get_all_possible_moves(self):
         moves = []
-        for r in range(self.board):
-            for c in range(self.board[r]):
-                if board[r][c] != '--':
-                    piece = board[r][c]
-                    color = piece[0]
-                    character = piece[1]
-
-                    if piece == 'p':
-                        self.get_pawn_moves(r, c, moves)
-                    elif piece == 'r':
-                        self.get_rook_moves(r, c, moves)
-                    elif piece == 'b':    
-                        self.get_bishop_moves(r, c, moves)
-                    elif piece == 'q':    
-                        self.get_queen_moves(r, c, moves)
-                    elif piece == 'n':    
-                        self.get_knight_moves(r, c, moves)
-                    elif piece == 'k':
-                        self.get_king_moves(r, c, moves)
-    return moves    
-
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                piece = self.board[r][c][1]
+                if (turn == 'w' and self.white_move) or (turn == 'b' and not self.white_move):
+                    self.move_functions[piece](r, c, moves)
+        return moves
     def get_pawn_moves(self, r, c, moves):
-        pass    
+        if self.white_move: #white moves go forward by subtraction
+            #blank move
+            if self.board[r - 1][c] == '--': #1 square move
+                moves.append(move((r,c),(r-1,c), self.board))
+                if r == 6 and self.board[r - 2][c] == '--': #2 square move
+                    moves.append(move((r,c),(r-2,c),self.board))
+            
+            #capture moves. can only capture diagonally and cant go off the board
+            if c-1 >= 0:
+                if self.board[r-1][c-1][0] == 'b':
+                    moves.append(move((r,c),(r-1,c-1),self.board))
+            if c+1 < len(self.board):   
+                if self.board[r-1][c+1][0] == 'b':
+                    moves.append(move((r,c),(r-1,c+1),self.board))
+
+        if not self.white_move:
+            if self.board[r + 1][c] == '--':
+                moves.append(move((r,c),(r+1,c), self.board))
+                if r == 1 and self.board[r + 2][c] == '--':
+                    moves.append(move((r,c),(r+2,c), self.board))
+            if c-1 >= 0:
+                if self.board[r+1][c-1][0] == 'w':
+                    moves.append(move((r,c),(r+1,c-1),self.board))
+            if c+1 < len(self.board):    
+                if self.board[r+1][c+1][0] == 'w':
+                    moves.append(move((r,c),(r+1,c+1),self.board))
+
     def get_rook_moves(self, r, c, moves):
-        pass
+        pass 
     def get_bishop_moves(self, r, c, moves):
         pass
     def get_queen_moves(self, r, c, moves):
         pass
     def get_knight_moves(self, r, c, moves):
-        pass
+        if self.white_move:        
+            #up 2 left 1 x
+            #up 2 right 1 x
+            #down 2 right 1 x
+            #down 2 left 1 x
+            #left 2 down 1 x
+            #left 2 up 1 x
+            #right 2 up 1 x
+            #right 2 down 1
+
+            if c + 1 < 7:
+                if r-2>=0:
+                    if self.board[r-2][c+1]=='--' or self.board[r-2][c+1][0]=='b':
+                        moves.append(move((r,c),(r-2,c+1),self.board))
+                if r+2 < len(self.board[0]):
+                    if self.board[r+2][c+1]=='--' or self.board[r-2][c+1][0]=='b':
+                        moves.append(move((r,c),(r+2,c+1),self.board))
+                    
+            if c - 1 >= 0:
+                if r-2>=0:
+                    if self.board[r-2][c-1]=='--' or self.board[r-2][c-1][0]=='b':
+                        moves.append(move((r,c),(r-1,c-1),self.board))
+                if r+2 < len(self.board[0]):
+                    if self.board[r+2][c-1]=='--' or self.board[r-2][c-1][0]=='b':
+                        moves.append(move((r,c),(r+2,c-1),self.board))
+            if c + 2 < 7:
+                if r-1>=0:
+                    if self.board[r-1][c+2]=='--' or self.board[r-1][c+2][0]=='b':
+                        moves.append(move((r,c),(r-1,c+2),self.board))
+                if r+1 < len(self.board[0]):
+                    if self.board[r+1][c+2]=='--' or self.board[r+1][c+2][0]=='b':
+                        moves.append(move((r,c),(r+1,c+2),self.board))
+            if c - 2 >= 0:
+                if r-1>=0:
+                    if self.board[r-1][c-2]=='--' or self.board[r-1][c-2][0]=='b':
+                        moves.append(move((r,c),(r-1,c-2),self.board))
+                if r+1 < len(self.board[0]):
+                    if self.board[r+1][c-2]=='--' or self.board[r+1][c-2][0]=='b':
+                        moves.append(move((r,c),(r+1,c-2),self.board))
+        if not self.white_move:        
+            if c + 1 < 7:
+                if r-2 < len(self.board[0]):
+                    if self.board[r-2][c+1]=='--' or self.board[r-2][c+1][0]=='b':
+                        moves.append(move((r,c),(r-2,c+1),self.board))
+                if r+2 >= 0:
+                    if self.board[r+2][c+1]=='--' or self.board[r-2][c+1][0]=='b':
+                        moves.append(move((r,c),(r+2,c+1),self.board))
+                    
+            if c - 1 >= 0:
+                if r-2 < len(self.board[0]):
+                    if self.board[r-2][c-1]=='--' or self.board[r-2][c-1][0]=='b':
+                        moves.append(move((r,c),(r-1,c-1),self.board))
+                if r+2 >= 0:
+                    if self.board[r+2][c-1]=='--' or self.board[r-2][c-1][0]=='b':
+                        moves.append(move((r,c),(r+2,c-1),self.board))
+            if c + 2 < 7:
+                if r-1 < len(self.board[0]):
+                    if self.board[r-1][c+2]=='--' or self.board[r-1][c+2][0]=='b':
+                        moves.append(move((r,c),(r-1,c+2),self.board))
+                if r+1 >= 0:
+                    if self.board[r+1][c+2]=='--' or self.board[r+1][c+2][0]=='b':
+                        moves.append(move((r,c),(r+1,c+2),self.board))
+            if c - 2 >= 0:
+                if r-1 < len(self.board[0]):
+                    if self.board[r-1][c-2]=='--' or self.board[r-1][c-2][0]=='b':
+                        moves.append(move((r,c),(r-1,c-2),self.board))
+                if r+1 >= 0:
+                    if self.board[r+1][c-2]=='--' or self.board[r+1][c-2][0]=='b':
+                        moves.append(move((r,c),(r+1,c-2),self.board))
+            
     def get_king_moves(self, r, c, moves):
-        pass
+        #left 1
+            #down 1
+            #same row 
+            #up 1
+        #right 1
+            #down 1
+            #same row
+            #up 1
+        if self.white_move: #white moves go forward by subtraction
+            if self.board[r - 1][c] == '--' or self.board[r-1][c]=='b':
+                moves.append(move((r,c),(r-1,c), self.board)) 
+            if c-1 >= 0:
+                if r-1>=0:
+                    if self.board[r-1][c-1][0] == 'b' or self.board[r-1][c-1]=='--':
+                        moves.append(move((r,c),(r-1,c-1),self.board))
+                if r+1<len(self.board[0]):
+                    if self.board[r+1][c-1] == '--' or self.board[r+1][c-1][0] == 'b':
+                        moves.append(move((r,c),(r+1,c-1), self.board))
+            if c+1 < len(self.board):   
+                if r-1>=0:    
+                    if self.board[r-1][c+1][0] == 'b' or self.board[r-1][c+1]=='--':
+                        moves.append(move((r,c),(r-1,c+1),self.board))
+                if r+1<len(self.board[0]): 
+                    if self.board[r+1][c+1]=='--' or self.board[r+1][c+1][0]=='b':
+                         moves.append(move((r,c),(r-1,c+1),self.board))
+
+        if not self.white_move:
+            if self.board[r + 1][c] == '--':
+                moves.append(move((r,c),(r+1,c), self.board))
+                if r == 1 and self.board[r + 2][c] == '--':
+                    moves.append(move((r,c),(r+2,c), self.board))
+            if c-1 >= 0:
+                if self.board[r+1][c-1][0] == 'w':
+                    moves.append(move((r,c),(r+1,c-1),self.board))
+            if c+1 < len(self.board):    
+                if self.board[r+1][c+1][0] == 'w':
+                    moves.append(move((r,c),(r+1,c+1),self.board))
+
+
+ #       if self.white_move:
+ #           if c-1>=0:
+ #               if r-1<=0:
+ #                   if self.board[r-1][c-1]=='--' or self.board[r-1][c-1]=='b':
+ #                       moves.append(move((r,c),(r-1,c-1), self.board))
+ #               if r+1 < len(self.board[0]):
+ #                   if self.board[r+1][c-1]=='--' or self.board[r+1][c-1]=='b':
+ #                       moves.append(move((r,c),(r+1,c-1),self.board))
+#                if self.board[r][c-1]=='--' or self.board[r][c-1]=='b':
+ #                   moves.append(move((r,c),(r,c-1),self.board))
+  #          if c+1<7:
+   #             if r-1>=-0:
+    #                if self.board[r-1][c+1]=='--' or self.board[r-1][c+1]=='b':
+     #                   moves.append(move((r,c),(r-1,c+1), self.board))
+      #          if r+1<len(self.board[0]):
+       #                 if self.board[r+1][c+1]=='--' or self.board[r+1][c+1]=='b':
+        #                    moves.append(move((r,c),(r+1,c+1),self.board))
+         #       if self.board[r][c+1]=='--' or self.board[r][c+1]=='b':
+          #          moves.append(move((r,c),(r,c+1),self.board))
 
 class move():
     #map key values 
@@ -93,7 +225,15 @@ class move():
 
         self.piece_moved = board[self.start_square_row][self.start_square_column]
         self.piece_captured = board[self.end_square_row][self.end_square_column]
-    
+            
+        self.moveID = self.start_square_row * 1000 + self.start_square_column * 100 + self.end_square_row * 10 + self.end_square_column
+
+    "overriding equal"
+    def __eq__(self, other):
+        if isinstance(other, move):
+            return self.moveID == other.moveID
+        return False    
+
     def get_chess_notation(self):
         return self.get_rank_file(self.start_square_row, self.start_square_column) + self.get_rank_file(self.end_square_row, self.end_square_column)
     def get_rank_file(self, row, column):
